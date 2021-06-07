c1b6b4ad26cd2366fabc39b02ac34356e968be48
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2021-06-07 22:49:05 +0300

    add blockNumber

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
+import '@typechain/hardhat';
 
 import { HardhatUserConfig } from 'hardhat/types';
 
@@ -9,6 +10,7 @@ const config: HardhatUserConfig = {
       forking: {
         url:
           'https://eth-mainnet.alchemyapi.io/v2/DMEoI146uTzBp_VdlGhwCVO-5tt7wdWM',
+        blockNumber: 12588624, // https://etherscan.io/tx/0x30ba41af8432c3e5260b1fce195b4c507c9d51463b760a2f2d9a8ddba70b582c
       },
     },
     rinkeby: {
