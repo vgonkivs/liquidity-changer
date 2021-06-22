40a762f8268c595e3a9bfd9165aaf678d6f87c48
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2021-06-22 19:24:52 +0300

    add linter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,13 +4,26 @@ import '@typechain/hardhat';
 
 import { HardhatUserConfig } from 'hardhat/types';
 
+const OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.7.6',
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 2_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
         url:
           'https://eth-mainnet.alchemyapi.io/v2/DMEoI146uTzBp_VdlGhwCVO-5tt7wdWM',
-        //  blockNumber: 12588624, // https://etherscan.io/tx/0x30ba41af8432c3e5260b1fce195b4c507c9d51463b760a2f2d9a8ddba70b582c
+           blockNumber: 12670105  , // https://etherscan.io/tx/0x30ba41af8432c3e5260b1fce195b4c507c9d51463b760a2f2d9a8ddba70b582c
       },
     },
     rinkeby: {
@@ -21,7 +34,7 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    version: '0.7.6',
+    compilers: [OPTIMIZER_COMPILER_SETTINGS],
     settings: {
       outputSelection: {
         '*': {
